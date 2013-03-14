01c07d1046cdfecc8f9df0b9643b5b44485dc24a
John Manero <jmanero@dyn.com>
2013-03-14 10:28:33 -0400

    Some flow-control tests

--- tests/03-control.js
+++ tests/03-control.js
@@ -1,6 +1,87 @@
 var Assert = require('assert');
 var Queue = require('../index');
 
-describe.skip("Control", function() {
-	it("hasn't had tests written yet...");
+describe("Control", function() {
+	var flood = 16;
+	var drain = 5;
+
+	var queue = new Queue({
+		paused : true,
+		flood : flood,
+		drain : drain
+	});
+
+	queue.worker = function(task, done) {
+		done(null, {
+			sum : task.a + task.b
+		});
+	};
+
+	var tasks = [], count = 500, shouldBe = 0;
+	for ( var i = 0; i < count; i++) {
+		tasks.push({
+			a : Math.floor(Math.random() * 6) + 1,
+			b : Math.floor(Math.random() * 6) + 1
+		});
+	}
+
+	describe("Setup", function() {
+		it("new queue has correct initial values", function() {
+			Assert.ok(queue.paused, "Not paused");
+			Assert.equal(queue.flood, flood, "Wrong flood threshold");
+			Assert.equal(queue.drain, drain, "Wrong drain threshold");
+		});
+		it(count + " test tasks have been generated", function() {
+			Assert.equal(tasks.length, count);
+		});
+	});
+	
+	describe("Concurrency", function() {
+		var concurrency = 16;
+		queue.concurrency = concurrency;
+		
+		it("should run " + concurrency + " parallel tasks", function() {
+			queue.push(tasks.splice(0, 64));
+			queue.resume();
+			
+			process.nextTick(function() {
+				Assert.equal(queue.running, concurrency);
+			});
+		});
+	});
+	
+	describe("Callbacks", function() {
+		it("should call the task's callback", function(done) {
+			queue.buffer(tasks.shift(), function(err, res) {
+				if(err) {
+					done(err);
+					return;
+				}
+				
+				done();
+			});
+		});
+	});
+	
+	describe("Worker", function() {
+		it("should use an overriding work functon", function(done) {
+			queue.buffer(tasks.shift(), function(err, res) {
+				if(err) {
+					done(err);
+					return;
+				}
+				
+				try {
+					Assert.notEqual(typeof res.diff, "undefined");
+					done();
+				} catch(e) {
+					done(e);
+				}
+			}, null, function(task, done) {
+				done(null, {
+					diff : task.a - task.b
+				});
+			});
+		});
+	});
 });
