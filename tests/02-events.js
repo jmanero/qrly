38649d9d7f0a6b99ce345c25b5898c523fbbeaca
John Manero <jmanero@dyn.com>
2013-03-14 09:52:32 -0400

    More tests for emitted events

--- tests/02-events.js
+++ tests/02-events.js
@@ -28,7 +28,7 @@ describe("Events", function() {
 	describe("Setup", function() {
 		it("new queue has correct initial values", function() {
 			Assert.ok(queue.paused, "Not paused");
-			Assert.equal(queue.flood, flood, "Wrong flood threshold")
+			Assert.equal(queue.flood, flood, "Wrong flood threshold");
 			Assert.equal(queue.drain, drain, "Wrong drain threshold");
 		});
 		it(count + " test tasks have been generated", function() {
@@ -70,6 +70,7 @@ describe("Events", function() {
 						try {
 							Assert.equal(queue.tasks.length, drain - 1, "Backlog is not equal to drain: "
 									+ queue.tasks.length);
+							shouldBe = drain - 1;
 							done();
 						} catch (e) {
 							done(e);
@@ -80,12 +81,85 @@ describe("Events", function() {
 		});
 	});
 
-	describe.skip("Event: flushed", function() {
-		it("emits 'flushed' after all tasks complete", function() {
-
+	describe("Event: flushed", function() {
+		it("emits 'flushed' with results after all tasks complete", function(done) {
+			queue.pause();
+			queue.clear();
+			queue.flushable = true;
+			queue.collect = true;
+			queue.cleanup = true;
+			queue.push(tasks.splice(0, 48));
+			shouldBe += 48;
+			
+			queue.resume();
+			
+			queue.once('flushed', function(res) {
+				try {
+					Assert.equal(queue.tasks, 0, "Tasks are still backlogged");
+					Assert.equal(queue.running, 0, "Tasks are still running");
+					Assert.equal(res.length, shouldBe, "Didn't return the right results: " + res.length);
+					shouldBe = 0;
+					done();
+				} catch(e) {
+					done(e);
+				}
+			});
 		});
-		it("doesn't emit 'flushed' when flushable is false", function() {
-
+		
+		it("clears the results list when cleanup is true", function() {
+			Assert.equal(queue.results.length, 0);
+		});
+		
+		it("doesn't save resutls when collect is false", function(done) {
+			queue.pause();
+			queue.clear();
+			queue.flushable = true;
+			queue.collect = false;
+			queue.push(tasks.splice(0, 48));
+			shouldBe += 48;
+			
+			queue.resume();
+			
+			queue.once('flushed', function(res) {
+				try {
+					Assert.equal(queue.tasks, 0, "Tasks are still backlogged");
+					Assert.equal(queue.running, 0, "Tasks are still running");
+					Assert.equal(queue.results.length, 0, "Didn't return the right results: " + queue.results.length);
+					shouldBe = 0;
+					done();
+				} catch(e) {
+					done(e);
+				}
+			});
+		});
+		
+		it("doesn't emit 'flushed' when flushable is false", function(done) {
+			queue.pause();
+			queue.clear();
+			queue.flushable = false;
+			queue.collect = true;
+			queue.push(tasks.splice(0, 48));
+			shouldBe += 48;
+			
+			queue.resume();
+			
+			var to = setTimeout(function() {
+				try {
+					Assert.equal(queue.tasks, 0, "Tasks are still backlogged");
+					Assert.equal(queue.running, 0, "Tasks are still running");
+					Assert.equal(queue.results.length, shouldBe, "Wrong number of results: " + queue.results.length);
+					shouldBe = 0;
+					
+					done();
+				} catch(e) {
+					done(e);
+				}
+			}, 24);
+			
+			queue.once('flushed', function() {
+				clearTimeout(to);
+				done(new Error("Flushed anyway..."));
+			});
 		});
 	});
 });
